f57d8c287dad002b60cfe735188eb24c340ea2dc
JennyViannay <jenny.viannay75@gmail.com>
2022-02-08 15:07:52 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+type CounterProps = {
+  initialCount: number;
+  step?: number;
+}
+
+const Counter = ({ initialCount = 0, step = 1 } : CounterProps) => {
+  const [count, setCount] = React.useState(initialCount);
+  const [increment, setIncrement] = React.useState(step);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <input type="number" onChange={(e) => setIncrement(parseInt(e.target.value))}/>
+      <button onClick={() => setCount(count - increment)}>-</button>
+      <button onClick={() => setCount(count + increment)}>+</button>
+    </div>
+  )
+}
+
+
+type FormProps = {
+  title: string;
+}
+
+const Form = ({ title } : FormProps) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      form
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter initialCount={0} />
+      <Form title="plop" />
     </div>
   );
 }
